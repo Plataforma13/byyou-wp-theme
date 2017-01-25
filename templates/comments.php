74193ebdbcead316ef784008f7a5a79b1f79296e
Eric Tonussi <erictonussi@gmail.com>
2017-01-25 12:52:38 -0200

    total de comentarios

--- templates/comments.php
+++ templates/comments.php
@@ -6,7 +6,7 @@ if (post_password_required()) {
 
 <section id="comments" class="post-comments">
   <?php if (have_comments()) : ?>
-    <h2><?php printf(_nx('One response to &ldquo;%2$s&rdquo;', '%1$s responses to &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'sage'), number_format_i18n(get_comments_number()), '<span>' . get_the_title() . '</span>'); ?></h2>
+    <h2 class="total-comments">Comentários (<?=get_comments_number()?>)</h2>
 
     <?php //comment_form(); ?>
     <div class="reply">
