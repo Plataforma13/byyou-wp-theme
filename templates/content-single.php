8240bab0825c43e729510a161d68850e599134ee
Eric Tonussi <erictonussi@gmail.com>
2017-01-25 13:17:17 -0200

    dist

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,7 +1,9 @@
 <?php while (have_posts()) : the_post(); ?>
   <article <?php post_class(); ?>>
     <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
+      <h2 class="entry-category"><?= get_categories()[0]->name; ?></h2>
+      <?php the_post_thumbnail('large');?>
+      <h2 class="entry-title"><?php the_title(); ?></h2>
       <?php get_template_part('templates/entry-meta'); ?>
     </header>
     <div class="entry-content">
--- templates/content.php
+++ templates/content.php
@@ -1,7 +1,6 @@
 <article <?php post_class(); ?>>
   <header>
     <h2 class="entry-category"><a href="<?php the_permalink(); ?>"><?= get_categories()[0]->name; ?></a></h2>
-    <!-- <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2> -->
     <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('large');?></a>
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <?php get_template_part('templates/entry-meta'); ?>
