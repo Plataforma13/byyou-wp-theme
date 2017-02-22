0deb6d1f5e46a2e43d52dc5600af22b5cbff980e
Eric Tonussi <erictonussi@gmail.com>
2017-02-22 18:02:45 -0300

    ajuste da listagem de posts

--- lib/extras.php
+++ lib/extras.php
@@ -31,3 +31,10 @@ function excerpt_more() {
   return ' &hellip; <a href="' . get_permalink() . '">' . __('Leia mais', 'sage') . '</a>';
 }
 add_filter('excerpt_more', __NAMESPACE__ . '\\excerpt_more');
+
+function wpdocs_custom_excerpt_length( $length ) {
+  // echo $length;
+  // die($length);
+  return 30;
+}
+add_filter( 'excerpt_length',  __NAMESPACE__ . '\\wpdocs_custom_excerpt_length', 999 );
